578faeb40fe1465d53976d4f272438da733bc496
Raymon S <raymon@raymonschouwenaar.nl>
2020-12-19 15:43:04 +0100

    Fix image optimisation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     images: {
-        domains: ['cdn-images-1.medium.com', 'a.impactradius-go.com', 'skillshare.eqcm.net']
-    }
+        domains: ['lh3.googleusercontent.com', 'avatars2.githubusercontent.com', 'res.cloudinary.com', 'localhost', 'cdn-images-1.medium.com', 'a.impactradius-go.com', 'skillshare.eqcm.net'],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+        loader: 'cloudinary',
+        path: 'https://res.cloudinary.com/raymons/image/fetch/',
+    },
+
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -55,10 +55,6 @@ export default function PostPage({ source, frontMatter }) {
 
       </div>
       <style jsx>{`
-        .post-header h1 {
-          margin-bottom: 0;
-        }
-
         .post-header {
           margin-bottom: 2rem;
         }
