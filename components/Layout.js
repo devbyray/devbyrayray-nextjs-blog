677fc3fdedcdce16e5a983ddd97c924db593707f
Raymon S <raymon@raymonschouwenaar.nl>
2020-12-29 20:12:53 +0100

    Add correct urls and cookie bot script

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,13 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 export default function Layout({ children }) {
-  let domain = process?.env?.DOMAIN || ''
-  console.log("🚀 ~ file: Layout.js ~ line 7 ~ Layout ~ domain", domain)
-
   return (
     <>
       <Head>
-        {domain !== 'localhost' && <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="280c2a50-1f41-457a-949b-fc68560eecf4" data-blockingmode="auto" type="text/javascript"></script>}
-
+        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="280c2a50-1f41-457a-949b-fc68560eecf4" data-blockingmode="auto" type="text/javascript"></script>
       </Head>
       <div className="relative pb-12 md:text-lg">{children}</div>
-      {domain !== 'localhost' && <script id="CookieDeclaration" src="https://consent.cookiebot.com/280c2a50-1f41-457a-949b-fc68560eecf4/cd.js" type="text/javascript" async></script>} 
+      <script id="CookieDeclaration" src="https://consent.cookiebot.com/280c2a50-1f41-457a-949b-fc68560eecf4/cd.js" type="text/javascript" async></script>
     </>
   )
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -145,10 +145,10 @@ export default function PostPage({ source, frontMatter }) {
                   'https://www.amazon.com/gp/product/1118907442/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1118907442&linkCode=as2&tag=devbyrayray09-20&linkId=eab1691ffa86c74f779aa39aceea817d'
                 }
                 image={
-                  'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=1118907442&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=devbyrayray09-20'
+                  'https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=1118907442&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=devbyrayray09-20'
                 }
                 tracking={
-                  'http://ir-na.amazon-adsystem.com/e/ir?t=devbyrayray09-20&l=am2&o=1&a=111890744'
+                  'https://ir-na.amazon-adsystem.com/e/ir?t=devbyrayray09-20&l=am2&o=1&a=111890744'
                 }
               />
             </div>
@@ -159,10 +159,10 @@ export default function PostPage({ source, frontMatter }) {
                   'https://www.amazon.com/gp/product/1491952024/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1491952024&linkCode=as2&tag=devbyrayray09-20&linkId=82851d38754d89040329ae7bf0f525d1'
                 }
                 image={
-                  'http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=1491952024&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=devbyrayray09-20'
+                  'https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=1491952024&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=devbyrayray09-20'
                 }
                 tracking={
-                  'http://ir-na.amazon-adsystem.com/e/ir?t=devbyrayray09-20&l=am2&o=1&a=1491952024'
+                  'https://ir-na.amazon-adsystem.com/e/ir?t=devbyrayray09-20&l=am2&o=1&a=1491952024'
                 }
               />
 
