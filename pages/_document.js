c6f298235225b22ebc8be28d17108b79ef4762ff
Raymon S <raymon@raymonschouwenaar.nl>
2021-01-07 20:47:35 +0100

    Add adsense script

--- pages/_document.js
+++ pages/_document.js
@@ -56,6 +56,7 @@ export default class CustomDocument extends Document {
                                     `,
                                 }}
                             />
+                            <script data-ad-client="ca-pub-0395200862879086" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                         </Fragment>
                     )}
                 </Head>
