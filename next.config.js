20dc9b82edfa47ec576865f5d2a400f2cfa96620
Raymon S <raymon@raymonschouwenaar.nl>
2021-03-12 19:47:09 +0100

    Fixed build issue

--- next.config.js
+++ next.config.js
@@ -70,6 +70,19 @@ const config = {
             },
         ]
     },
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.mdx/,
+            use: [
+                options.defaultLoaders.babel,
+                {
+                    loader: '@mdx-js/loader',
+                },
+            ],
+        })
+
+        return config
+    },
 }
 
 module.exports = config
