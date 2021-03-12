ce263c75dade84c44c33387716e9b9f792ec51de
Raymon S <raymon@raymonschouwenaar.nl>
2021-03-12 19:48:18 +0100

    Add serverless option

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 
 const config = {
+    target: "serverless",
     images: {
         domains: ['lh3.googleusercontent.com', 'avatars2.githubusercontent.com', 'res.cloudinary.com', 'localhost', 'cdn-images-1.medium.com', 'a.impactradius-go.com', 'skillshare.eqcm.net', 'cloudways.com', 'ws-na.amazon-adsystem.com', 'ir-na.amazon-adsystem.com'],
         deviceSizes: [640, 750, 828, 1080, 1200, 1920],
